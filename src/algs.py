1379353158822755a0b07d1f308f381823a5c729
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-06-04 16:53:55 +0100

    Fix Q-Learning to work with environment

--- src/algs.py
+++ src/algs.py
@@ -2,22 +2,23 @@ import env
 import numpy as np
 import random
 
+
 def qlearn(environment):
     qtable = np.zeros((environment.observation_space.n, environment.action_space.n))
     print(qtable)
 
     # the total number of episodes to run
     total_episodes = 200
-    
+
     # the maximum number of steps per episode
     max_steps = 99
-    
+
     # the learning rate
     learning_rate = 0.8
-    
+
     # the discount factor
     gamma = 0.95
-    
+
     # the range for the exploration parameter epsilon
     epsilon = 1.0
     min_epsilon = 0.01
@@ -28,43 +29,59 @@ def qlearn(environment):
 
     rewards = []
 
+    """
+    if neutral 0
+    if new L +10
+    if same L -10
+    if end and all visited +50
+    if end and not all visited -25
+    """
+
     for episode in range(total_episodes):
-        state = environment.reset()
+        environment.reset()
+        state = environment.start_state
         print(f"state: {state}")
-        step = 0 
         done = False
         total_rewards = 0
-
-        for step in range(max_steps):
-            print(f"start step...")
+        for _ in range(max_steps):
+            idx = environment.size * state[0] + state[1]
             exp_exp_tradeoff = random.uniform(0, 1)
             print(f"exp_exp_tradeoff: {exp_exp_tradeoff}")
 
             if exp_exp_tradeoff > epsilon:
-                print(f"qtable[state,:] {qtable[state,:]}")
-                action = np.argmax(qtable[state,:])
+                action = np.argmax(qtable[idx, :])
             else:
                 action = environment.action_space.sample()
                 print(f"action is {action}")
-            
-            new_state, reward, done, info = environment.step(action)
-            print(f"new_state: {new_state}, reward: {reward}, done: {done}, info: {info}")
-            qtable[state, action] = qtable[state, action] + learning_rate * (reward + gamma * np.max(qtable[new_state, :]) - qtable[state, action])
 
-            print(f'qtable: {qtable}')
+            new_state, reward, done, info = environment.step(state, action)
+
+            print(f"The new state in q-learning is {new_state}")
+
+            new_idx = environment.size * new_state[0] + new_state[1]
+            print(
+                f"new_state: {new_state}, reward: {reward}, done: {done}, info: {info}"
+            )
+            qtable[idx, action] = qtable[idx, action] + learning_rate * (
+                reward + gamma * np.max(qtable[new_idx, :]) - qtable[idx, action]
+            )
+
+            print(f"qtable: {qtable}")
 
             total_rewards = total_rewards + reward
-            print(f'total_rewards {total_rewards}')
+            print(f"total_rewards {total_rewards}")
 
             state = new_state
 
-            if done: 
+            environment.render()
+            if done:
                 break
 
-        epsilon = min_epsilon + (max_epsilon - min_epsilon)*np.exp(-decay_rate*episode)
+        epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(
+            -decay_rate * episode
+        )
         rewards.append(total_rewards)
 
-    print ("Score/time: " +  str(sum(rewards)/total_episodes))
+    print("Score/time: " + str(sum(rewards) / total_episodes))
     print(qtable)
     print(epsilon)
-        
