491c6c435fe3391a2dbc51d3ea48d3c1b043bedf
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-06-04 17:09:46 +0100

    Update env to include more complex board

--- src/env.py
+++ src/env.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from typing import Tuple
+from typing import List, Set, Tuple
 from gym import Env, spaces
 import numpy as np
 
@@ -19,18 +19,33 @@ class TakeTheLEnv(Env):
     metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 50}
 
     def __init__(self) -> None:
-        self.board: np.ndarray = np.zeros([BOARD_ROWS, BOARD_COLS], dtype=np.int32)
+        self.board: List[List[int]] = [
+            [1, 1, 1, 2, 2, 0],
+            [0, 3, 1, 2, 4, 0],
+            [0, 3, 0, 2, 4, 5],
+            [0, 3, 3, 4, 4, 5],
+            [0, 6, 6, 6, 5, 5],
+            [0, 0, 0, 6, 0, 0],
+        ]
         self.size: int = len(self.board)
         self.start_state: Tuple[int, int] = (self.size - 1, 0)
         self.goal_state: Tuple[int, int] = (0, self.size - 1)
-        self.board[self.start_state[0]][self.start_state[1]] = 1
+        self.visited = set()
+        self.all_shapes = self.determine_shapes(self.board)
 
         # Gym specific variables
         self.action_space: spaces.Discrete = spaces.Discrete(4)
         self.observation_space: spaces.Discrete = spaces.Discrete(self.size**2)
 
     def reset(self) -> None:
-        self.board = np.zeros([BOARD_ROWS, BOARD_COLS], dtype=np.int32)
+        self.board = [
+            [1, 1, 1, 2, 2, 0],
+            [0, 3, 1, 2, 4, 0],
+            [0, 3, 0, 2, 4, 5],
+            [0, 3, 3, 4, 4, 5],
+            [0, 6, 6, 6, 5, 5],
+            [0, 0, 0, 6, 0, 0],
+        ]
 
     # NOTE: For mow it is using print. Later change to pygame
     def render(self) -> None:
@@ -57,8 +72,6 @@ class TakeTheLEnv(Env):
         else:
             new_state = state
 
-        self.board[new_state[0]][new_state[1]] = 1
-
         done: bool = self.reached_end(new_state)
         reward: float = self.determine_reward(new_state)
         info: dict = {}
@@ -74,19 +87,56 @@ class TakeTheLEnv(Env):
         )
 
     def determine_reward(self, state) -> float:
-        if self.board[state[0]][state[1]] == 0:
+        val = self.board[state[0]][state[1]]
+        if self.reached_end(state) and self.all_visited():
+            return 50
+        if self.reached_end(state) and (not self.all_visited()):
+            return -25
+        if val not in self.visited:
+            return 10
+        if val in self.visited:
+            return -10
+        else:
             return 0
 
+    def determine_shapes(self, board: List[List[int]]) -> Set[int]:
+        """
+        Determines shapes from board content
+        Parameters:
+        board (list[list[int]]): the content of the board
+
+        Returns:
+        shapes (set): the set of different shapes in the board
+        """
+        final = set()
+        for line in board:
+            for num in line:
+                if num not in final:
+                    final.add(num)
+        return final
+
+    def all_visited(self):
+        """
+        Checks if the player has visited all the different shapes
+        Parameters:
+            board (Board): the board's content
+
+        Return:
+            all_visited (bool): whether the player has visited all the shapes
+        """
+        return self.visited == self.all_shapes
+
+    # TODO: Change to fit new board style
     def showBoard(self) -> None:
         for i in range(0, BOARD_ROWS):
             print("-----------------")
             out = "| "
             for j in range(0, BOARD_COLS):
-                if self.board[i, j] == 1:
+                if self.board[i][j] == 1:
                     token = "*"
-                elif self.board[i, j] == -1:
+                elif self.board[i][j] == -1:
                     token = "z"
-                elif self.board[i, j] == 0:
+                elif self.board[i][j] == 0:
                     token = "0"
                 else:
                     token = " "
