e6be7e4f11b65065e7dd9286d1ceac8dd51cfc7c
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-06-05 22:18:02 +0100

    Try to implement DQN but to no avail

--- src/test.py
+++ src/test.py
@@ -1,5 +1,6 @@
 import numpy
 from stable_baselines3.dqn.dqn import DQN
+from stable_baselines3.common.evaluation import evaluate_policy
 
 from new_env import TakeTheLEnv
 
@@ -7,16 +8,18 @@ from new_env import TakeTheLEnv
 def main():
     print("DQN")
     env = TakeTheLEnv()
-    model = DQN("MlpPolicy", env)
+    model = DQN("MlpPolicy", env=env, verbose=1)
     model.learn(total_timesteps=10000)
     obs = env.reset()
 
     for _ in range(1000):
-        action, _ = model.predict(numpy.asarray(obs), deterministic=True)
-        obs, _, done, _ = env.step(action)
-        env.set_state(env.from_idx(obs))
-        env.render()
+        action, _ = model.predict(obs, deterministic=True)
+        new_obs, _, done, _ = env.step(action)
+        obs = new_obs
+        env.set_state(env.from_idx(new_obs))
+
         if done == True:
+            env.render()
             obs = env.reset()
             break
     # action, _ = model.predict(numpy.asarray(state), deterministic=True)
